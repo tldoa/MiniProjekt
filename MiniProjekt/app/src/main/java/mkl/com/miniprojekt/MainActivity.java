040b8822de680e6979af85ea536e379485350d3d
Mattias Madsen <tldoa@live.dk>
2017-09-21 10:46:29 +0200

    ny

--- MiniProjekt/app/src/main/java/mkl/com/miniprojekt/MainActivity.java
+++ MiniProjekt/app/src/main/java/mkl/com/miniprojekt/MainActivity.java
@@ -1,7 +1,11 @@
 package mkl.com.miniprojekt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_add_shop:
+                Intent intent = new Intent(this, AddShopActivity.class);
+                startActivity(intent);
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
