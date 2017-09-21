cde33afda49b0ce8a88e314898ef8d1f9a068c54
Laukess <LukasHansen89@gmail.com>
2017-09-21 10:48:40 +0200

    before

--- MiniProjekt/app/src/main/java/mkl/com/miniprojekt/MainActivity.java
+++ MiniProjekt/app/src/main/java/mkl/com/miniprojekt/MainActivity.java
@@ -1,7 +1,12 @@
 package mkl.com.miniprojekt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Log.d("TIS", "tis tis tis 2");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d("TIS", "tis tis tis");
+        switch (item.getItemId()) {
+            case R.id.action_add_shop:
+                Intent intent = new Intent(this, AddShopActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
 }
